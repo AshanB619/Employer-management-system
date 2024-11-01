fc42e3d9aaabe3db9f3d8ab2d53e9a9704138dec
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-11-01 09:02:51 +0530

    update

--- emplyer_mngt_project/src/main/java/com/example/emplyer_mngt_project/EmplyerMngtProjectApplication.java
+++ emplyer_mngt_project/src/main/java/com/example/emplyer_mngt_project/EmplyerMngtProjectApplication.java
@@ -8,6 +8,7 @@ public class EmplyerMngtProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmplyerMngtProjectApplication.class, args);
-	}
+		System.out.println("Hello World!");}
+
 
 }
