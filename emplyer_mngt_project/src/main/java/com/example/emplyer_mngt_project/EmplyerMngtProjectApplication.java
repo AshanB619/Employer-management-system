2b79a630633b19010019d9ca1f390b46ce850c2c
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-11-01 09:01:18 +0530

    update

---
+++ emplyer_mngt_project/src/main/java/com/example/emplyer_mngt_project/EmplyerMngtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.emplyer_mngt_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplyerMngtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplyerMngtProjectApplication.class, args);
+	}
+
+}
---
+++ emplyer_mngt_project/src/test/java/com/example/emplyer_mngt_project/EmplyerMngtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emplyer_mngt_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyerMngtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
