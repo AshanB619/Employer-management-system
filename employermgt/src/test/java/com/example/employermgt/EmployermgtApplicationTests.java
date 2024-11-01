a1cff482ab980f3784c6159a3ce08fd9d4ec3917
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-11-01 11:14:09 +0530

    update
    update

--- employermgt/src/test/java/com/example/employermgt/EmployermgtApplicationTests.java
+++ employermgt/src/test/java/com/example/employermgt/EmployermgtApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.emplyer_mngt_project;
+package com.example.employermgt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmplyerMngtProjectApplicationTests {
+class EmployermgtApplicationTests {
 
 	@Test
 	void contextLoads() {
